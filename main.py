0345f7f35581611f453477ff9b76730eb39fb0bc
Mehmet Selman Baysan <mselmanbaysan@gmail.com>
2022-12-12 18:39:57 +0300

    comments added

--- main.py
+++ main.py
@@ -40,51 +40,51 @@ def get_remaining_peg(board):
     return sum(x.count(1) for x in board)
 
 def create_peg_solitaire_board():
-    board = []
-    for row in range(7):
-        board.append([])
-        for col in range(7):
-            if row in [0,1,5,6] and col in [0,1,5,6]:
-                board[row].append(-1)
-            elif (row == 3 and col == 3):
-                board[row].append(0)
-            else:
-                board[row].append(1)
-    return board
+    board = [] # create an empty list
+    for row in range(7): # iterate through the rows
+        board.append([]) # add an empty list to the board
+        for col in range(7): # iterate through the columns
+            if row in [0,1,5,6] and col in [0,1,5,6]: # if the position is out of the board, add -1 to the board
+                board[row].append(-1) 
+            elif (row == 3 and col == 3): # if the position is in the middle of the board, add 0 to the board
+                board[row].append(0) 
+            else: # if the position is in the board, add 1 to the board
+                board[row].append(1)  
+    return board # return the board
 
 def check_if_board_is_solved(board):
     """Return True if there are only 1 piece left on the board, else return False"""
-    peg_number = sum(x.count(1) for x in board)
-    if peg_number == 1 and board[3][3] == 1:
+    peg_number = sum(x.count(1) for x in board) # count the number of pegs in the board
+    if peg_number == 1 and board[3][3] == 1: # if there is only 1 peg left and it is in the middle of the board, return True
         return True
-    return False
+    return False # else return False
 
 def move_peg(board, start, end):
-    new_board = list(map(list, board))
-    if (start[0] == end[0] and abs(start[1] - end[1]) == 2) or (start[1] == end[1] and abs(start[0] - end[0]) == 2):
-        if new_board[start[0]][start[1]] == 1 and new_board[end[0]][end[1]] == 0:
-            if start[0] == end[0]:
-                mid = (start[0], (start[1] + end[1]) // 2)
-            else:
-                mid = ((start[0] + end[0]) // 2, start[1])
-            if new_board[mid[0]][mid[1]] == 1:
-                new_board[start[0]][start[1]] = 0
-                new_board[mid[0]][mid[1]] = 0
-                new_board[end[0]][end[1]] = 1
-                return new_board
-    return False
+    new_board = list(map(list, board)) # create a copy of the board
+    if (start[0] == end[0] and abs(start[1] - end[1]) == 2) or (start[1] == end[1] and abs(start[0] - end[0]) == 2): # check if the move is valid
+        if new_board[start[0]][start[1]] == 1 and new_board[end[0]][end[1]] == 0: # check if the start and end positions are valid
+            if start[0] == end[0]: # if the move is horizontal
+                mid = (start[0], (start[1] + end[1]) // 2) # calculate the middle position
+            else: # if the move is vertical
+                mid = ((start[0] + end[0]) // 2, start[1]) # calculate the middle position
+            if new_board[mid[0]][mid[1]] == 1: # check if the middle position is valid
+                new_board[start[0]][start[1]] = 0 # move the peg
+                new_board[mid[0]][mid[1]] = 0 # remove the peg that was jumped over
+                new_board[end[0]][end[1]] = 1 # place the peg in the new position
+                return new_board # return the new board
+    return False # if the move is not valid, return False
 
 def get_possible_moves(board):
-    moves = []
-    for row in range(7):
-        for col in range(7):
-            if board[row][col] == 1:
-                if row > 1 and board[row - 1][col] == 1 and board[row - 2][col] == 0:
-                    moves.append(((row, col), (row - 2, col)))
-                if row < 5 and board[row + 1][col] == 1 and board[row + 2][col] == 0:
-                    moves.append(((row, col), (row + 2, col)))
-                if col > 1 and board[row][col - 1] == 1 and board[row][col - 2] == 0:
-                    moves.append(((row, col), (row, col - 2)))
-                if col < 5 and board[row][col + 1] == 1 and board[row][col + 2] == 0:
-                    moves.append(((row, col), (row, col + 2)))
-    return moves
+    moves = [] # list to store the possible moves
+    for row in range(7): # iterate through the board
+        for col in range(7): # iterate through the board
+            if board[row][col] == 1: # if the position is a peg
+                if row > 1 and board[row - 1][col] == 1 and board[row - 2][col] == 0: # check if the move is valid
+                    moves.append(((row, col), (row - 2, col))) # if it is, add it to the list
+                if row < 5 and board[row + 1][col] == 1 and board[row + 2][col] == 0: # check if the move is valid
+                    moves.append(((row, col), (row + 2, col))) # if it is, add it to the list
+                if col > 1 and board[row][col - 1] == 1 and board[row][col - 2] == 0: # check if the move is valid
+                    moves.append(((row, col), (row, col - 2))) # if it is, add it to the list
+                if col < 5 and board[row][col + 1] == 1 and board[row][col + 2] == 0: # check if the move is valid
+                    moves.append(((row, col), (row, col + 2))) # if it is, add it to the list
+    return moves # return the list of possible moves
